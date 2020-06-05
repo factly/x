de1057c94b4e5d26794b9f4e636abecf4737fe72
shreeharsha-factly <harsha@factly.in>
2020-06-05 13:17:17 +0530

    pagination is added

---
+++ pagination/parse.go
@@ -0,0 +1,27 @@
+package pagination
+
+import (
+	"net/url"
+	"strconv"
+)
+
+// Parse - pagination
+func Parse(query url.Values) (int, int) {
+
+	page, _ := strconv.Atoi(query.Get("page"))     // page number
+	perPage, _ := strconv.Atoi(query.Get("limit")) // perPage number
+
+	offset := 0 // no. of records to skip
+	limit := 5  // limit
+
+	if perPage > 0 && perPage <= 10 {
+		limit = perPage
+	}
+
+	if page > 1 {
+		offset = (page - 1) * limit
+	}
+
+	return offset, limit
+
+}
