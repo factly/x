0265228f1e8fc4e451b12d210dd96d052dd82353
hetjagani <hetpatel572@gmail.com>
2021-02-07 14:18:33 -0800

    Fix: passed ref to ref in slugx.Approve

--- slugx/slug.go
+++ slugx/slug.go
@@ -11,7 +11,7 @@ type comman struct {
 }
 
 // Approve return slug
-func Approve(db *gorm.DB, slug string, space int, table string) string {
+func Approve(db **gorm.DB, slug string, space int, table string) string {
 	var result []comman
 	(*db).Table(table).Select("slug, space_id").Where("slug LIKE ? AND space_id = ? AND deleted_at IS NULL", slug+"%", space).First(&result)
 	count := 0
