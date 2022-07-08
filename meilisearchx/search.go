84c39df76d6ae4312883ae47cbede67c22777357
vsumit89 <sumit.vishwakarma@factly.in>
2022-07-08 18:46:02 +0530

    added kind and appended it to filters

--- meilisearchx/search.go
+++ meilisearchx/search.go
@@ -1,61 +1,19 @@
 package meilisearchx
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
-	"net/http"
 	"strings"
 
 	meilisearch "github.com/meilisearch/meilisearch-go"
-	"github.com/spf13/viper"
 )
 
-// SearchWithoutQuery calls meili without q
-func SearchWithoutQuery(indexName string, filters string, kind string) (map[string]interface{}, error) {
-
-	body := map[string]interface{}{
-		"filters":      filters,
-		"facetFilters": []string{"kind:" + kind},
-		"limit":        1000000,
-	}
-
-	buf := new(bytes.Buffer)
-	err := json.NewEncoder(buf).Encode(&body)
-	if err != nil {
-		return nil, err
-	}
-
-	req, err := http.NewRequest("POST", viper.GetString("meili_url")+"/indexes/"+indexName+"/search", buf)
-	req.Header.Add("X-Meili-API-Key", viper.GetString("meili_key"))
-	req.Header.Add("Content-Type", "application/json")
-
-	if err != nil {
-		return nil, err
-	}
-
-	client := &http.Client{}
-	res, err := client.Do(req)
-
-	if err != nil {
-		return nil, err
-	}
-
-	var result map[string]interface{}
-
-	err = json.NewDecoder(res.Body).Decode(&result)
-	if err != nil {
-		return nil, err
-	}
-
-	return result, nil
-}
-
 // SearchWithQuery calls meili with q
-func SearchWithQuery(indexName, q, filters string) ([]interface{}, error) {
+func SearchWithQuery(indexName, q, filters, kind string) ([]interface{}, error) {
+	filter := [][]string{}
+	filter = append(filter, []string{filters}, []string{fmt.Sprintf("kind=%s", kind)})
 	result, err := Client.Index(indexName).Search(q, &meilisearch.SearchRequest{
-		Filter:      filters,
-		Limit:        1000000,
+		Filter: filter,
+		Limit:  1000000,
 	})
 
 	if err != nil {
